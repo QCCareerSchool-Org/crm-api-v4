2b7c0c021b809c3a0b2b774e59eadfccfc47ace9
Dave Welsh <dave@qccareerschool.com>
2022-07-15 17:21:02 -0400

    Add get student controller and route

--- src/frameworks/express/studentRouter.ts
+++ src/frameworks/express/studentRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { AddPaymentMethodController } from '../../controllers/students/addPaymentMethodController.js';
+import { GetStudentController } from '../../controllers/students/getStudentController.js';
 import { StudentGuardMiddleware } from '../../controllers/students/studentGuardMiddleware.js';
 import type { Route } from './applyRoutes.js';
 import { applyRoutes } from './applyRoutes.js';
@@ -9,6 +10,7 @@ export const studentRouter = Router();
 
 const routes: Route[] = [
   [ 'use', '/:studentId', StudentGuardMiddleware ],
+  [ 'get', '/:studentId', GetStudentController ],
   [ 'post', '/:studentId/enrollments/:enrollmentId/paymentMethods', AddPaymentMethodController ],
 ];
 
--- src/interactors/students/index.ts
+++ src/interactors/students/index.ts
@@ -1,5 +1,7 @@
 import { prisma } from '../../frameworks/prisma/index.js';
 import { paysafeServiceFactory, winstonLoggerService } from '../../services/index.js';
 import { AddPaymentMethodInteractor } from './addPaymentMethodInteractor.js';
+import { GetStudentInteractor } from './getStudentInteractor.js';
 
 export const addPaymentMethodInteractor = new AddPaymentMethodInteractor(prisma, paysafeServiceFactory, winstonLoggerService);
+export const getStudentInteractor = new GetStudentInteractor(prisma, winstonLoggerService);
